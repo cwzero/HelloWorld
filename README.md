f6215bc1b166cb11cfc0bfed16bb9af89b2beaf7
Eric Gjerde <eric.gjerde@mymail.nwtc.edu>
2015-09-03 15:21:08 -0500

    Hello World project, initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # HelloWorld
 Hello World for Java Class at NWTC
+
+This is the hello world assignment from day one of Programming 3 (Java)
+
+Currently consists of a single class: com.liquidforte.java.HelloWorld
+With a single method: public static void HelloWorld.main(String[] args)
+An entry point method that simply writes "Hello, World!" to the standard console.
---
+++ src/com/liquidforte/java/HelloWorld.java
@@ -0,0 +1,27 @@
+package com.liquidforte.java;
+
+/**
+ * HelloWorld.java - HelloWorld class<br />
+ * Because we are robots 0_0
+ * 
+ * @author Eric Gjerde
+ * @version 1.0.0 September 3, 2015
+ * 
+ */
+public class HelloWorld {
+
+	// Make this private to remove it from javadoc //
+	private HelloWorld() {
+	}
+
+	/**
+	 * The main method, do the heavy lifting (just prints "Hello, world!")
+	 * 
+	 * @param args
+	 *            The command line arguments/parameters
+	 */
+	public static void main(String[] args) {
+		// Say hello! //
+		System.out.println("Hello, world!");
+	}
+}
